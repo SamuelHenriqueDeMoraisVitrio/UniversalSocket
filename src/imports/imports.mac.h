6de3011db01d107dcb46c3f0872f4fc2ff3a4672
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-20 19:39:04 -0300

    removing imports

--- src/imports/imports.mac.h
+++
@@ -1,7 +0,0 @@
-#include "imports.dep.h"
-#ifndef UniversalSocket_mac
-#define UniversalSocket_mac
-#include "../all/mac.macros.h"
-#include "../linux/mac.macros.h"
-#include "../windows/mac.macros.h"
-#endif
--- src/imports/imports.types.h
+++
@@ -1,7 +0,0 @@
-#include "imports.mac.h"
-#ifndef UniversalSocket_types
-#define UniversalSocket_types
-#include "../all/types.socket.h"
-#include "../linux/types.socket.h"
-#include "../windows/types.socket.h"
-#endif
