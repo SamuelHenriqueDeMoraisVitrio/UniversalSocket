aa98d0f2bc52ae9fefe1577b112ae4ac31977db5
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-02-20 22:28:05 -0300

    added

--- build/main.lua
+++ build/main.lua
@@ -12,5 +12,30 @@ function main()
     local MAX_RECURSION = 100
 
     local one = darwin.camalgamator.generate_amalgamation("src/imports/imports.def.h", MAX_CONTENT, MAX_RECURSION)
+    local lincense = "/* " .. darwin.dtw.load_file("LICENSE") .. " */ \n"
+    one = lincense .. one
     darwin.dtw.write_file("UniversalSocketOne.h", one)
+
+    local only_declare = darwin.camalgamator.generate_amalgamation("src/imports/imports.dec.h", MAX_CONTENT,
+        MAX_RECURSION)
+    only_declare = lincense .. only_declare
+
+    darwin.dtw.write_file("UniversalSocket.h", only_declare)
+
+    local only_definition = darwin.camalgamator.generate_amalgamation_with_callback("src/imports/imports.def.h",
+        function(import, path)
+            if darwin.dtw.ends_with(import, ".c") then
+                return "include-once"
+            end
+            return "dont-include"
+        end,
+        MAX_CONTENT,
+        MAX_RECURSION
+    )
+    only_definition = '#include "UniversalSocket.h"\n' .. only_definition
+
+    only_definition = lincense .. only_definition
+
+
+    darwin.dtw.write_file("UniversalSocket.c", only_definition)
 end
