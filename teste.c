078074f27b237a97e78ec57b33c3639fa46bc0ab
Mateus Moutinho Queiroz <61179749+mateusmoutinho@users.noreply.github.com>
2025-03-08 00:12:21 -0300

    att

--- teste.c
+++ teste.c
@@ -54,7 +54,7 @@ int main() {
         "\r\n"
         "Hello, World!";
 
-   Universal_send(client_fd,response, sizeof(response),MSG_WAITALL);
+   Universal_send(client_fd,response, sizeof(response)-1,MSG_WAITALL);
 
     // Close server socket
     Universal_close(server_fd);
